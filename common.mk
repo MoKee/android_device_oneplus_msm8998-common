9cb3c1cda2f8a378b10361316153c4ff68cc1361
XiNGRZ <chenxingyu92@gmail.com>
2017-10-07 02:25:50 +0800

    HAX: Kill SOTER
    Change-Id: I9d9452c1d6168a5972d88dc49f403e151dd62696

--- common.mk
+++ common.mk
@@ -360,11 +360,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 # SOTER (Fingerprint support for WeChat Payment)
-PRODUCT_PACKAGES += \
-    soter
+# PRODUCT_PACKAGES += \
+#     soter
 
-PRODUCT_BOOT_JARS += \
-    soter
+# PRODUCT_BOOT_JARS += \
+#     soter
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
