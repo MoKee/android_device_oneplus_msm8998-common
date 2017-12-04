9b43b0a5808fa667c3b3572f889ae7829f74fed3
Hamster Tian <haotia@gmail.com>
2017-12-05 00:04:57 +0800

    cheeseburger: add qcnvitems and qcrilhook to BOOT_JARS
    * Needed to get QtiTelephonyService loaded. Fixes in-call audio.
    
    Change-Id: I7dbfc015c42e90f1c9a5c66fa93cea252e7ef73c

--- device.mk
+++ device.mk
@@ -379,6 +379,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=false
 
+PRODUCT_BOOT_JARS += \
+    qcnvitems \
+    qcrilhook
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
