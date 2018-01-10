726867af90cba50735de2e5a3fb38c8725109d17
XiNGRZ <chenxingyu92@gmail.com>
2018-01-11 01:44:31 +0800

    msm8998-common: Update Wi-Fi configs
    Change-Id: I0fe522ea79ab2dabf5109fcd881baa504a5647a2

--- common.mk
+++ common.mk
@@ -355,6 +355,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     soter
 
+# Tetheroffload
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
@@ -382,9 +387,8 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    ipacm \
-    ipacm-diag \
-    IPACM_cfg.xml \
+    libqsap_sdk \
+    libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
     wificond \
