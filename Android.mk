cd4f0391d1bb5d4933b3408410adf05fbb84a777
Hamster Tian <haotia@gmail.com>
2018-01-14 00:02:13 +0800

    msm8998-common: allow cheeseburger to the party
    Change-Id: Ic72403f022eea1bdf9d9eaf501ef90f2984ad1a7

--- Android.mk
+++ Android.mk
@@ -27,7 +27,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter dumpling,$(TARGET_DEVICE)),)
+ifneq ($(filter cheeseburger dumpling,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
