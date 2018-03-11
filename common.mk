de5ea1d45269f701188ea33966020bf941f08600
XiNGRZ <chenxingyu92@gmail.com>
2018-03-11 10:50:49 +0800

    msm8998: Remove custom camera provier HAL and update blobs form OB6
    * OnePlus finally implemented camera vndbinder for OB6
    
    Change-Id: I4a4492dfb6d268d2d631b16137ed224759cbc37e

--- common.mk
+++ common.mk
@@ -162,7 +162,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.oneplus5 \
+    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
