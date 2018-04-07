05c3d8c0f02a1f68b83ffec1fa7e1ae37818e5fb
XiNGRZ <chenxingyu92@gmail.com>
2018-04-07 17:34:16 +0800

    msm8998: fsconfig: Remove duplicated items
    Change-Id: I5b3fe31b76a32eae70e183e6985492b5f6f9213d

--- config.fs
+++ config.fs
@@ -31,25 +31,7 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[system/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
 [system/bin/cnss-daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[system/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -60,24 +42,6 @@ mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
-
-[system/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
-[system/vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
 
 [system/vendor/bin/qvrservice]
 mode: 0755
