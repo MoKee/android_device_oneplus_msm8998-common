66e91246155c43b4a4c5aceb78450381bc88a43e
XiNGRZ <chenxingyu92@gmail.com>
2018-04-23 22:59:09 +0800

    msm8998: Copy QDCM calib data files to vendor
    * There's no reason to define them as modules
    * They should be in /vendor/etc instead of /system/etc
    
    Change-Id: I5ea5ddba040b5c2cf0dfa43196924d006961e8b1

--- common.mk
+++ common.mk
@@ -212,10 +212,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
-PRODUCT_PACKAGES += \
-    qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml \
-    qdcm_calib_data_samsung_s6e3fa6_1080p_cmd_mode_dsi_panel.xml \
-    qdcm_calib_data_samsung_s6e3fc1_cmd_mode_dsi_panel.xml
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/display/qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml \
+    $(LOCAL_PATH)/display/qdcm_calib_data_samsung_s6e3fa6_1080p_cmd_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_samsung_s6e3fa6_1080p_cmd_mode_dsi_panel.xml \
+    $(LOCAL_PATH)/display/qdcm_calib_data_samsung_s6e3fc1_cmd_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_samsung_s6e3fc1_cmd_mode_dsi_panel.xml
 
 # DRM
 PRODUCT_PACKAGES += \
