848af015061c28ea668c4c94ed4ad69a51473453
Hamster Tian <haotia@gmail.com>
2018-09-22 16:05:09 +0800

    msm8998-common: add open source GPS libraries
    gps/ from android-9.0.0_r6 with following commits:
    
    (change id from CAF)
    add pos tech mask and sv source info I069a4564da7b96e29e5305b126cd88db0c426bc4
    added new ulp type for raw gnss data I5ebc4a8f10bd52de4e59e29dcf28d6375df02d73
    Decouple position inject request from assist data I4c2f8439dca9ea3b4d56a3cd00da88e4488717bf
    Add vertical uncertainity field I2610b0eaf527b516e7a1cac958b82795f535cc06
    
    gps-impl/ from sonyxperiadev/vendor-qcom-opensource-location @ a21df4a
    
    Change-Id: I4fa2fb50a9529bad310cdc5d5028bfc96c2eba2f

--- gps/utils/LocTimer.cpp
+++ gps/utils/LocTimer.cpp
@@ -284,8 +284,7 @@ void LocTimerContainer::updateSoonestTime(LocTimerDelegate* priorTop) {
 
     // check if top has changed
     if (curTop != priorTop) {
-        struct itimerspec delay;
-        memset(&delay, 0, sizeof(struct itimerspec));
+        struct itimerspec delay = {0};
         bool toSetTime = false;
         // if tree is empty now, we remove poll and disarm timer
         if (!curTop) {
@@ -376,8 +375,7 @@ void LocTimerContainer::expire() {
         }
     };
 
-    struct itimerspec delay;
-    memset(&delay, 0, sizeof(struct itimerspec));
+    struct itimerspec delay = {0};
     timerfd_settime(getTimerFd(), TFD_TIMER_ABSTIME, &delay, NULL);
     mPollTask->removePoll(*this);
     mMsgTask->sendMsg(new MsgTimerExpire(*this));
